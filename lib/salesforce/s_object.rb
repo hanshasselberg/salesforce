0b91c476bb45b92996e00e0d7378fb29d557b7c5
Hans Hasselberg <me@hans.io>
2011-10-28 18:08:04 +0200

    more s_object stuff.

--- lib/salesforce/s_object.rb
+++ lib/salesforce/s_object.rb
@@ -4,7 +4,25 @@ module Salesforce
     module ClassMethods
 
       def where(options = {})
-        [1]
+        ids = [options[:id]].flatten
+        []
+      end
+
+      def type
+        'sfAccount'
+      end
+
+      private
+
+      def object_url(id)
+        Salesforce.configuration.service_url + "/sobjects/#{self.type}/#{id}"
+      end
+
+      def request(url)
+        response = Typhoeus::Request.get(
+          configuration.service_url + url,
+          :headers => { 'Authorization' => "OAuth #{configuration.access_token}" }
+        )
       end
 
     end
--- spec/unit/salesforce/s_object_spec.rb
+++ spec/unit/salesforce/s_object_spec.rb
@@ -4,11 +4,34 @@ describe Salesforce::SObject do
 
   let(:klass) { Account }
 
+  describe '.type' do
+
+    specify { Account.type.should == 'sfAccount'}
+
+  end
+
+  describe '.object_url' do
+
+    context 'given an id' do
+      let(:service_url) { "http://service.url" }
+
+      specify do
+        Salesforce.configuration.stub(:service_url){ service_url }
+        result_url = service_url+"/sobjects/#{Account.type}/123"
+
+        Account.send(:object_url, '123').should == result_url
+      end
+
+    end
+
+  end
+
   describe '.where' do
 
-    context 'give a valid id' do
+    context 'given a valid id' do
 
-      it 'finds an object' do
+      it 'finds an sobject' do
+        pending
         klass.where(:id => '123').should include(klass)
       end
 
