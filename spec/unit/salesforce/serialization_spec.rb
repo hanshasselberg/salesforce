9c1c85f0630500b148ce8b582a428f3de8cca8cf
Hans Hasselberg <me@hans.io>
2011-11-03 16:18:39 +0100

    more serialization tests

--- spec/unit/salesforce/serialization_spec.rb
+++ spec/unit/salesforce/serialization_spec.rb
@@ -4,16 +4,25 @@ describe Salesforce::Serialization do
 
   let(:klass) { Account }
   let(:account) { klass.new }
+  let(:json) { "{\"account\":{\"Id\":\"123\",\"Website\":\"www.example.com\"}}" }
 
-  describe '#serializable_hash' do
+  context 'given field Id and Website' do
 
-    context 'given field Id and Website' do
+    before do
+      klass.field('Id'); klass.field('Website')
+      account.id = '123'
+      account.website = 'www.example.com'
+    end
 
-      before do
-        klass.field('Id'); klass.field('Website')
-        account.id = '123'
-        account.website = 'www.example.com'
+    describe '#to_json' do
+
+      it 'returns proper json' do
+        account.to_json.should eq(json)
       end
+
+    end
+
+    describe '#serializable_hash' do
 
       it 'returns hash with keys: Id and CreatedById' do
         account.serializable_hash.should include('Id', 'Website')
