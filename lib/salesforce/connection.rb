e952409370cbf8ca09675b167a8ec659f93fb757
Hans Hasselberg <me@hans.io>
2011-11-09 17:23:12 +0100

    fixes #13

--- lib/salesforce/connection.rb
+++ lib/salesforce/connection.rb
@@ -17,32 +17,28 @@ module Salesforce
       end
 
       def request_object(id)
-        new request_data(object_url(id))
+        new request(object_url(id))
       end
 
       def description
-        @ressource_description ||= request_data(description_url)
+        @ressource_description ||= request(description_url)
       end
 
       def persist_object(o)
         # send_data o
       end
 
-      private
-
-      def send_data(o)
-        method = o.new_record ? :post : :put
-        response = request(url, :method => method, :body => o.serializable_hash.to_json.to_s)
-        body = JSON.parse(response.body)
-        unless response.code == 201
-          raise SalesforceError.new("#{body.first['errorCode']}: #{body.first['message']}")
-        end
-        o.id = body['id']
-        o
-      end
-
-      def request_data(url)
-        response = request(url, :method => :get)
+      def request(url, options = {})
+        options[:method] ||= :get
+        options[:headers] ||= {}
+        options[:headers].merge!(default_headers)
+        options[:headers].merge!(auth_headers) if not options.delete(:no_auth)
+        # options.merge! :verbose => true
+        request = Typhoeus::Request.new(url, options)
+        hydra = Typhoeus::Hydra.hydra
+        hydra.queue(request)
+        hydra.run
+        response = request.response
         body = JSON.parse(response.body)
         unless response.code == 200
           raise SalesforceError.new("#{body.first['errorCode']}: #{body.first['message']}")
@@ -50,25 +46,20 @@ module Salesforce
         body
       end
 
-      def request(url, options = {})
-        request = Typhoeus::Request.new(url, options.merge(headers))
-        hydra = Typhoeus::Hydra.new
-        hydra.queue(request)
-        hydra.run
-        request.response
-      end
-
-      def headers
-        {:headers =>
-          {
-            'Authorization' => "OAuth #{Salesforce.configuration.access_token}",
-            'Content-Type' => 'application/json; charset=UTF-8',
-            'Accept' => 'application/json'
-          }
+      def default_headers
+        {
+          'Content-Type' => 'application/json; charset=UTF-8',
+          'Accept' => 'application/json',
+          'X-PrettyPrint' => 1
         }
       end
 
+      def auth_headers
+        { 'Authorization' => "OAuth #{Salesforce.configuration.access_token}" }
+      end
+
     end
+    extend ClassMethods
 
   end
 end
