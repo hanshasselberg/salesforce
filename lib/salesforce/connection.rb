c26e3d222171018a314ebdaece5abc4eca9409a1
Hans Hasselberg <me@hans.io>
2011-11-13 22:50:40 +0100

    remove old debug out

--- lib/salesforce/connection.rb
+++ lib/salesforce/connection.rb
@@ -30,7 +30,6 @@ module Salesforce
         options[:headers].merge!(default_headers)
         options[:headers].merge!(auth_headers) if not options.delete(:no_auth)
         options.merge! :verbose => true
-        p "url: #{url}"
         request = Typhoeus::Request.new(url, options)
         hydra = Typhoeus::Hydra.hydra
         hydra.queue(request)
